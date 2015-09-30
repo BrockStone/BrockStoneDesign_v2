3a1fbd7d289c3764c450d0120b84e6796cff5a46
Brock Stone <brockstonedesign@gmail.com>
2015-09-29 23:32:05 -0400

    configured JsHint

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -1,21 +1,36 @@
-// wrapper function (required by grunt and its plugins)
-// all configuration goes inside this function
+// Wrapper function (required by grunt and its plugins)
+// All configuration goes inside this function
 module.exports = function(grunt) {
 
-  
-  // CONFIGURE GRUNT
+  // +*+*+*+*+*+*+*+*+*+*+*+*+
+  //     Configure Grunt
+  // +*+*+*+*+*+*+*+*+*+*+*+*+
+
   grunt.initConfig({
 
-    // get the configuration info from package.json 
-    // this way we can use things like name and version (pkg.name)
+    // Get the configuration info from package.json this
+    // way we can use things like name and version (pkg.name)
     pkg: grunt.file.readJSON('package.json'),
 
-    // all of our configuration will go here
+    // +*+*+*+*+*+*+*+*+*+*+*+*+
+    //      Configurations
+    // +*+*+*+*+*+*+*+*+*+*+*+*+
 
+    // Configure jshint to validate js files --->
+    jshint: {
+      options: {
+        reporter: require('jshint-stylish') // use jshint-stylish to make our errors look and read good
+      },
+
+      // When this task is run, lint the Gruntfile and all js files in src
+      build: ['Gruntfile.js', 'src/**/*.js']
+    }
   });
 
-  // LOAD GRUNT PLUGINS
-  
+  // +*+*+*+*+*+*+*+*+*+*+*+*+
+  //    Load Grunt Plugins
+  // +*+*+*+*+*+*+*+*+*+*+*+*+
+
   // we can only load these if they are in our package.json
   // make sure you have run npm install so our app can find these
   grunt.loadNpmTasks('grunt-contrib-jshint');
